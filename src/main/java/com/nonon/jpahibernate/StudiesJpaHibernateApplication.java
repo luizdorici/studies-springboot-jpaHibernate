34dc27f5b84e81356bb199f7fe26c4795da301cd
Luiz Dorici <luizdorici@gmail.com>
2021-01-22 14:11:54 -0300

    first commit

---
+++ src/main/java/com/nonon/jpahibernate/StudiesJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.nonon.jpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiesJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiesJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nonon/jpahibernate/StudiesJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nonon.jpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiesJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
