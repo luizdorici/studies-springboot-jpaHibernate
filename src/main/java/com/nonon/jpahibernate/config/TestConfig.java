e8229a40ac67f4d07fe8dd93a9812c5eb93dadb9
Luiz Dorici <luizdorici@gmail.com>
2021-02-03 12:14:16 -0300

    criando a associacao de pagamento

--- src/main/java/com/nonon/jpahibernate/config/TestConfig.java
+++ src/main/java/com/nonon/jpahibernate/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.nonon.jpahibernate.entities.Category;
 import com.nonon.jpahibernate.entities.Order;
 import com.nonon.jpahibernate.entities.OrderItem;
+import com.nonon.jpahibernate.entities.Payment;
 import com.nonon.jpahibernate.entities.Product;
 import com.nonon.jpahibernate.entities.User;
 import com.nonon.jpahibernate.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 }
